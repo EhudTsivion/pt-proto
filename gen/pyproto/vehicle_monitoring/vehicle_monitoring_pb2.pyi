1dbf24037fc853a4f0c72b0731921f36085a1cf9
DESKTOP-1RSSN52\tsivi <tsivion.ehud@gmail.com>
2026-08-12 20:52:39 +0300

    more changes

--- gen/pyproto/vehicle_monitoring/vehicle_monitoring_pb2.pyi
+++ gen/pyproto/vehicle_monitoring/vehicle_monitoring_pb2.pyi
@@ -1,7 +1,7 @@
 import datetime
 
 from google.protobuf import timestamp_pb2 as _timestamp_pb2
-from pt.common import types_pb2 as _types_pb2
+from common import types_pb2 as _types_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
